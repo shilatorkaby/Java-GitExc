3c00a3f8d09a97ade9aca49dc1c5b196d3a54b33
shilatorkaby <shillat14@gmail.com>
2022-10-30 15:10:00 +0200

    fill user class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,13 @@
 public class User {
+int id;
+String email;
+String password;
+String name;
 
-
+    public User(int id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
