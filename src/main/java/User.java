cf881322114faa454bed86428ef54e44eb2f322a
shilatorkaby <shillat14@gmail.com>
2022-10-30 16:49:58 +0200

    add some function

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,15 +1,19 @@
-//Neeed to get a token from Auth Service
+//Need to get a token from Auth Service
 public class User {
-int id;
+Integer id;
 String email;
 String password;
 String name;
 
-    public User(int id, String email, String password, String name) {
+    public User(Integer id, String email, String password, String name) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = name;
     }
 
+    String getPassword(){return password;};
+    String getEmail(){return email;};
+    String getName(){return name;};
+    Integer getId(){return id;};
 }
