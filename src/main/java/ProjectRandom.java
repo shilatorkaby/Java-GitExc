a186478f47c5454468f49e324325f0347d16e250
Rany Saliman <ranysaliman@gmail.com>
2022-10-31 15:24:11 +0200

    idk what i added but i have to commit

--- src/main/java/ProjectRandom.java
+++ src/main/java/ProjectRandom.java
@@ -9,15 +9,15 @@ public class ProjectRandom {
 
         Faker faker = new Faker();
 
-//        String getRandomName(){return faker.name().firstName();};
-//
-//        String getRandomEmail(){return(faker.name().firstName()+ ThreadLocalRandom.current().nextInt(100, 299 + 1)+"@gmail.com");}
-//
-//        IdNumber getRandomId(){return faker.idNumber();}
-//
-//        public String getFirstName() {
-//            return name;
-//        }
+        String getRandomName(){return faker.name().firstName();};
+
+        String getRandomEmail(){return(faker.name().firstName()+ ThreadLocalRandom.current().nextInt(100, 299 + 1)+"@gmail.com");}
+
+        IdNumber getRandomId(){return faker.idNumber();}
+
+        public String getFirstName() {
+            return faker.name().firstName();
+        }
 
     }
 
