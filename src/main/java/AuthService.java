fcce1e157d5ceafc5cc5648b18946a4a847b2ee3
shilatorkaby <shillat14@gmail.com>
2022-10-31 16:51:39 +0200

    add read and write from file functions

--- src/main/java/AuthService.java
+++ src/main/java/AuthService.java
@@ -1,41 +1,40 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
-
-public class AuthService implements AuthController {
-    Map<Integer,String> UserToken; //move to userRepository
-
-    public AuthService() {
-        UserToken = new HashMap<>();
-    }
-    //create user in user repository
-
-    //Validates user details after logging in
-    //change boolean to return token (string)
-    boolean validateLogin(String email,Integer id)
-    {
-        for (Map.Entry<Integer,String> entry : UserToken.entrySet())
-            if (entry.getValue()==email && entry.getKey()== id)
-                return true;
-        return false;
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-    @Override
-    public String getEmail() {
-        return null;
-    }
-
-    @Override
-    public String password() {
-        return null;
-    }
-
-    @Override
-    public boolean validateRequest(String email, String password) {
-        return false;
-    }
-}
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.concurrent.ThreadLocalRandom;
+//
+//public class AuthService implements AuthController {
+//
+//    public AuthService() {
+//        UserToken = new HashMap<>();
+//    }
+//    //create user in user repository
+//
+//    //Validates user details after logging in
+//    //change boolean to return token (string)
+//    boolean validateLogin(String email,Integer id)
+//    {
+//        for (Map.Entry<Integer,String> entry : UserToken.entrySet())
+//            if (entry.getValue()==email && entry.getKey()== id)
+//                return true;
+//        return false;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    @Override
+//    public String getEmail() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String password() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean validateRequest(String email, String password) {
+//        return false;
+//    }
+//}
