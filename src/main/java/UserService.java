4ae369742fb613ce7779e2476ae12a95d68592c5
Dalia <dalya1812@gmail.com>
2022-10-31 18:55:20 +0200

    try to solve error

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -4,10 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 //Can update user data (email/name/password) - via UserService
 //Can delete a user - via UserService
 public class UserService implements UserController {
-
     String loginToUser(String email, String password) {
         //get token (after validation) from auth-service by password and email
-        return "token"; //return token
+        // return "token"; //return token
     public void makeNewUser(String email, String password, String name) {
         User user = new User(email, password, name);
     }
