a88bf89ab68e9f825f3691524d2e0989ce9757ed
Dalia <dalya1812@gmail.com>
2022-10-31 16:51:29 +0200

    inialize token functions

--- src/main/java/ProjectRandom.java
+++ src/main/java/ProjectRandom.java
@@ -5,19 +5,19 @@ import com.github.javafaker.Pokemon;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProjectRandom {
-
-
         Faker faker = new Faker();
 
-//        String getRandomName(){return faker.name().firstName();};
-//
-//        String getRandomEmail(){return(faker.name().firstName()+ ThreadLocalRandom.current().nextInt(100, 299 + 1)+"@gmail.com");}
-//
-//        IdNumber getRandomId(){return faker.idNumber();}
-//
-//        public String getFirstName() {
-//            return name;
-//        }
+        String getRandomName(){return faker.name().firstName();};
+
+        String getRandomEmail(){return(faker.name().firstName()+ ThreadLocalRandom.current().nextInt(100, 299 + 1)+"@gmail.com");}
+
+
+
+    IdNumber getRandomId(){return faker.idNumber();}
+
+        public String getFirstName() {
+            return faker.name().firstName();
+        }
 
     }
 
--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 //Validates client requests
 //Can update user data (email/name/password) - via UserService
 //Can delete a user - via UserService
@@ -5,17 +7,22 @@ public class UserService implements UserController {
     public void makeNewUser(String email, String password, String name) {
         User user = new User(email, password, name);
     }
-
-
+    //check if user exist in map. if yes-> ask for get the exist token
+    //else generate token.
+    //get token (after validation) from auth-service by password and email
     String loginToUser(String email, String password) {
-        //get token (after validation) from auth-service by password and email
-        return "token"; //return token
+       User user = getUserByEmail(email);
+       if(checkIfUserInMap){
+           return getUserToken;
+       }
+       else {
+          return generateToken(user.id);
+       }
     }
+
 
     @Override
     public void updateEmail(String email,String token) {
-
-
     }
 
     @Override
@@ -32,5 +39,10 @@ public class UserService implements UserController {
     public void deleteUser(String email, String password) {
 
     }
+
+    private String generateToken(int id){
+        return String.valueOf(ThreadLocalRandom.current().nextInt(100000000, 999999999));
+    }
+
 }
 
