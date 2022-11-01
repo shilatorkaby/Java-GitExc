7f29ac39a73cb3b91d53dd94435d3a75a144353b
Dalia <dalya1812@gmail.com>
2022-11-01 17:41:54 +0200

    create user, start with tokens and login

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,26 +4,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 //Need to get a token from Auth Service
 class User {
-ProjectRandom random;
 final Integer id;
 String email;
 String name;
 String password;
 
+    ProjectRandom random = new ProjectRandom();
+
     public User(String email, String password, String name) {
-        this.id = random.generateNum();
+        this.id = random.getRandomIdTo1m();
         this.email = email;
         this.password = password;
         this.name = name;
     }
     public User(String password, String name) {
-        this.id= random.generateNum();
+        this.id= random.getRandomIdTo1m();
         this.email = random.getRandomEmail();
         this.password = password;
         this.name = name;
     }
     public User(String email) {
-        this.id= random.generateNum();
+        this.id= random.getRandomIdTo1m();
         this.email = email;
         this.password = random.getRandomPassword();
         this.name = random.getRandomName();
